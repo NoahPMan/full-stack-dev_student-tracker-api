b61113b905656cd9e82864c7e661e2a8dbcb7366
KBarts <kbarta@rrc.ca>
2026-01-19 17:32:00 -0600

    feat: add application footer component and integrate into layout

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="layout-footer">
+      <p>Code Trackers</p>
+      <ul>
+        <li>Noah Manaigre</li>
+        <li>Muniru Adam</li>
+        <li>Kaeden Barta</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
